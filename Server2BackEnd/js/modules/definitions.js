a3564bd2a63bd50883418a9d87ca858c63ea25a6
Mike Doswell <mike.doswell@gmail.com>
2025-02-06 11:38:11 -0800

    added server code

---
+++ Server2BackEnd/js/modules/definitions.js
@@ -0,0 +1,94 @@
+const url = require('url')
+const fs = require('fs')
+
+class Definitions {
+
+    constructor() {
+        this.dictionary = []
+        this.reqCount = 0
+        this.messages = JSON.parse(fs.readFileSync('./Server2BackEnd/lang/en/messages.json'))
+    }
+
+    handleRequest = (req, res) => {
+        this.reqCount++
+        if (req.method === "GET")
+            return this.handleGet(req, res)
+        if (req.method === "POST")
+            return this.handlePost(req, res)
+    }
+
+    handleGet = (req, res) => {
+        const urlParts = url.parse(req.url)
+        const params = new URLSearchParams(urlParts.query)
+        const word = params.get('word')
+        const dictEntry = this.searchDictionary(word)
+        const response = {}
+        response.reqestNumber = this.reqCount
+
+        if (dictEntry === null) {
+            response.error = this.messages.wordNotFound.replace("%1", word)
+            return this.sendResponse(res, 400, JSON.stringify(response))
+        }
+
+        response.word = word
+        response.defintion = dictEntry.definition
+        return this.sendResponse(res, 200, JSON.stringify(response))
+    }
+
+    handlePost = (req, res) => {
+        console.log("in handle post")
+        let query = ""
+        req.on("data", (chunk) => {
+            query += chunk
+        })
+        req.on("end", () => {
+            const body = JSON.parse(query)
+            if (!body.word || !body.definition) {
+                const response = {}
+                response.requestNumber = this.reqCount
+                response.error = this.messages.missingDefinition
+                return this.sendResponse(res, 400, JSON.stringify(response))
+            }
+            if (!this.addToDictionary(body.word, body.definition)) {
+                const response = {}
+                response.requestNumber = this.reqCount
+                response.error = this.messages.wordAlreadyExists.replace("%1", body.word)
+                return this.sendResponse(res, 400, JSON.stringify(response))
+            }
+            const response = {}
+            response.requestNumber = this.reqCount
+            response.message = this.messages.wordAdded.replace("%1", body.word)
+            return this.sendResponse(res, 200, JSON.stringify(response))
+        })
+
+        // return this.sendResponse(res, 200, JSON.stringify(response))
+        // return
+    }
+
+    sendResponse = (res, status, message) => {
+        res.writeHead(status, { 'content-type': "application/json" })
+        res.write(message)
+        res.end()
+    }
+
+    searchDictionary = (word) => {
+        for (let i = 0; i < this.dictionary.length; i++) {
+            if (this.dictionary[i].word === word)
+                return this.dictionary[i]
+        }
+        return null
+    }
+
+    addToDictionary = (word, definition) => {
+        for (let i = 0; i < this.dictionary.length; i++) {
+            if (this.dictionary[i].word === word)
+                return false
+        }
+
+        console.log("adding...")
+        this.dictionary.push({ word: word, definition: definition })
+        return true
+    }
+}
+
+module.exports = Definitions
---
+++ Server2BackEnd/js/server.js
@@ -0,0 +1,34 @@
+const http = require('http')
+const url = require('url')
+const fs = require('fs')
+const Definitions = require('./modules/definitions')
+
+class Server {
+
+    constructor() {
+        this.definitions = new Definitions();
+        const messages = JSON.parse(fs.readFileSync('./Server2BackEnd/lang/en/messages.json'))
+        this.notFound = messages.notFound
+        this.routes = [
+            {pathRegex: /^\/api\/definitions\/?$/, action: this.definitions.handleRequest}
+        ]
+    }
+
+    start() {
+        http.createServer((req, res) => {
+            const urlParts = url.parse(req.url)
+            for (let i = 0; i < this.routes.length; i++) {
+                if (this.routes[i].pathRegex.test(urlParts.pathname.toLowerCase())) {
+                    return this.routes[i].action(req, res)
+                }
+            }
+            res.writeHead(404, { 'content-type': 'text/html' })
+            res.end(this.notFound)
+        }).listen(8088)
+        
+        console.log('Server listening...')
+    }
+}
+
+const server = new Server();
+server.start();
