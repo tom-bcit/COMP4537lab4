00f8ec13cf8a79fdf455ad9170d6d0bede60f8bc
Mike Doswell <mike.doswell@gmail.com>
2025-02-08 12:29:39 -0800

    added try-catches for handling server errors

--- Server2BackEnd/modules/definitions.js
+++ Server2BackEnd/modules/definitions.js
@@ -20,52 +20,60 @@ class Definitions {
     }
 
     handleGet = (req, res) => {
-        const urlParts = url.parse(req.url)
-        const params = new URLSearchParams(urlParts.query)
-        const word = params.get('word')
-        const dictEntry = this.searchDictionary(word)
-        const response = {}
-        response.requestNumber = this.reqCount
+        try {
+            const urlParts = url.parse(req.url)
+            const params = new URLSearchParams(urlParts.query)
+            const word = params.get('word')
+            const dictEntry = this.searchDictionary(word)
+            const response = {}
+            response.requestNumber = this.reqCount
 
-        if (dictEntry === null) {
-            response.error = this.messages.wordNotFound.replace("%1", word)
-            return this.sendResponse(res, 400, JSON.stringify(response))
+            if (dictEntry === null) {
+                response.error = this.messages.wordNotFound.replace("%1", word)
+                return this.sendResponse(res, 404, JSON.stringify(response))
+            }
+
+            response.word = word
+            response.definition = dictEntry.definition
+            return this.sendResponse(res, 200, JSON.stringify(response))
+        } catch (err) {
+            const response = {}
+            response.error = this.messages.serverError + err
+            return this.sendResponse(res, 500, JSON.stringify(response))
         }
-
-        response.word = word
-        response.definition = dictEntry.definition
-        return this.sendResponse(res, 200, JSON.stringify(response))
     }
 
     handlePost = (req, res) => {
-        console.log("in handle post")
-        let query = ""
-        req.on("data", (chunk) => {
-            query += chunk
-        })
-        req.on("end", () => {
-            const body = JSON.parse(query)
-            if (!body.word || !body.definition) {
+        try {
+            let query = ""
+            req.on("data", (chunk) => {
+                query += chunk
+            })
+            req.on("end", () => {
+                const body = JSON.parse(query)
                 const response = {}
                 response.requestNumber = this.reqCount
-                response.error = this.messages.missingDefinition
-                return this.sendResponse(res, 400, JSON.stringify(response))
-            }
-            if (!this.addToDictionary(body.word, body.definition)) {
-                const response = {}
-                response.requestNumber = this.reqCount
-                response.error = this.messages.wordAlreadyExists.replace("%1", body.word)
-                return this.sendResponse(res, 400, JSON.stringify(response))
-            }
+                if (!body.word || !body.definition) {
+                    response.error = this.messages.missingDefinition
+                    return this.sendResponse(res, 400, JSON.stringify(response))
+                }
+                if (!this.addToDictionary(body.word, body.definition)) {
+                    response.error = this.messages.wordAlreadyExists.replace("%1", body.word)
+                    return this.sendResponse(res, 400, JSON.stringify(response))
+                }
+                response.message = this.messages.wordAdded.replace("%1", body.word)
+                return this.sendResponse(res, 200, JSON.stringify(response))
+            })
+        } catch (err) {
             const response = {}
-            response.requestNumber = this.reqCount
-            response.message = this.messages.wordAdded.replace("%1", body.word)
-            return this.sendResponse(res, 200, JSON.stringify(response))
-        })
+            response.error = this.messages.serverError + err
+            return this.sendResponse(res, 500, JSON.stringify(response))
+        }
+
     }
 
     sendResponse = (res, status, message) => {
-        res.writeHead(status, { 
+        res.writeHead(status, {
             'access-control-allow-methods': 'GET, POST, OPTIONS',
             'Access-Control-Allow-Origin': '*',
             'content-type': "application/json"
@@ -88,7 +96,6 @@ class Definitions {
                 return false
         }
 
-        console.log("adding...")
         this.dictionary.push({ word: word, definition: definition })
         return true
     }
